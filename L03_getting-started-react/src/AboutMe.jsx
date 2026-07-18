e240e871944bf17ea27edccdea794e257c595b6c
mandocdocmawin-dot <mandocdocmawin@gmail.com>
2026-07-18 14:57:38 +0800

    Done with laboratory 3

---
+++ L03_getting-started-react/src/AboutMe.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="profile-card">
+      <h1>Marwin Mandocdoc</h1>
+      <h2>Course and Year: BSIS 3</h2>
+
+      <h3>Fun Facts About Me:</h3>
+      <ul>
+        <li>Vide coder</li>
+        <li>Web developer</li>
+        <li>watching kdrama</li>
+      </ul>
+
+      <hr />
+      <h3>Course Reflection</h3>
+      <p className="reflection-text">
+        So far in this Information Systems course, I have learned the basics of building websites and writing code. Doing the previous activities helped me practice my problem-solving skills and learn new things. It was a bit challenging at first, but seeing my learning style is very rewarding. I want to continue improving my skills so I can reach my goal of becoming a good web developer in the future.
+      </p>
+    </div>
+  );
+}
+
+export default App;
